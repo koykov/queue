a91904cdc01139c29de5dc409274b74e839777a5
Konstantin Voykov <koykov0@gmail.com>
2020-11-20 00:12:52 +0200

    Improve draft.

--- bqueue.go
+++ bqueue.go
@@ -1,30 +1,65 @@
 package queue
 
+import "sync"
+
 const (
-	defaultWorkersLimit  = 8
-	newWorkerThreshold   = .75
-	sleepWorkerThreshold = .5
+	defaultWakeupFactor = .75
+	defaultSleepFactor  = .5
 )
 
 type BalancedQueue struct {
+	once   sync.Once
 	stream Stream
+	w      []*worker
+	wc     uint32
 
-	Worker                        Worker
-	WorkersInit, WorkersLimit, wc uint32
+	Size uint64
+
+	proc Proc
+
+	WakeupFactor float32
+	SleepFactor  float32
+
+	WorkersMin uint32
+	WorkersMax uint32
 }
 
 func (q *BalancedQueue) Put(x interface{}) bool {
-	// todo init queue once
 	q.rebalance()
 	q.stream <- x
 	return true
 }
 
+func (q *BalancedQueue) init() {
+	q.stream = make(Stream, q.Size)
+
+	if q.WorkersMin <= 0 {
+		q.WorkersMin = 1
+	}
+	if q.WorkersMax < q.WorkersMin {
+		q.WorkersMax = q.WorkersMin
+	}
+
+	if q.WakeupFactor <= 0 {
+		q.WakeupFactor = defaultWakeupFactor
+	}
+	if q.SleepFactor <= 0 {
+		q.SleepFactor = defaultSleepFactor
+	}
+	if q.WakeupFactor < q.SleepFactor {
+		q.WakeupFactor = q.SleepFactor
+	}
+}
+
 func (q *BalancedQueue) rebalance() {
+	if q.stream == nil {
+		q.once.Do(q.init)
+	}
+
 	rate := q.lcRate()
-	if rate >= newWorkerThreshold {
+	if rate >= q.WakeupFactor {
 		// todo make new worker or wakeup sleeping worker and use it Observe() method in new goroutine.
-	} else if rate <= sleepWorkerThreshold {
+	} else if rate <= q.SleepFactor {
 		// todo sleep one of active workers.
 	}
 }
--- queue.go
+++ queue.go
@@ -1,13 +1,27 @@
 package queue
 
+type signal uint
+type status uint
+
+const (
+	signalInit   signal = 0
+	signalSleep         = 1
+	signalResume        = 2
+	signalStop          = 3
+
+	statusActive status = 0
+	statusIdle          = 1
+	statusSleep         = 2
+)
+
 type Queuer interface {
 	Put(x interface{}) bool
 }
 
 type Stream chan interface{}
+type Proc func(x interface{})
 
-type Worker interface {
-	Observe(stream Stream)
-	Sleep()
-	Wakeup()
+type worker struct {
+	status status
+	proc   Proc
 }
