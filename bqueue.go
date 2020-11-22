6bf17de24d48fc34a729f8b06192739fde61358a
Konstantin Voykov <koykov0@gmail.com>
2020-11-22 23:51:03 +0200

    Queue rebalance issue.

--- bqueue.go
+++ bqueue.go
@@ -1,22 +1,31 @@
 package queue
 
-import "sync"
+import (
+	"sync"
+	"sync/atomic"
+	"time"
+)
 
 const (
 	defaultWakeupFactor = .75
 	defaultSleepFactor  = .5
+	defaultHeartbeat    = time.Millisecond
+
+	spinlockLimit = 1000
 )
 
 type BalancedQueue struct {
 	queue
-
-	once   sync.Once
 	stream stream
-	w      []*worker
-	c      []ctl
-	wc     uint32
+	Size   uint64
 
-	Size uint64
+	once sync.Once
+
+	mux      sync.Mutex
+	workers  []*worker
+	ctl      []ctl
+	workerUp uint32
+	spinlock uint32
 
 	proc Proc
 
@@ -25,11 +34,20 @@ type BalancedQueue struct {
 
 	WorkersMin uint32
 	WorkersMax uint32
+
+	Heartbeat time.Duration
 }
 
 func (q *BalancedQueue) Put(x interface{}) bool {
-	q.rebalance()
+	if q.status == qstatusNil {
+		q.once.Do(q.init)
+	}
+
+	if atomic.AddUint32(&q.spinlock, 1) >= spinlockLimit {
+		q.rebalance()
+	}
 	q.stream <- x
+	atomic.AddUint32(&q.spinlock, -1)
 	return true
 }
 
@@ -53,29 +71,39 @@ func (q *BalancedQueue) init() {
 		q.WakeupFactor = q.SleepFactor
 	}
 
-	q.w = make([]*worker, q.WorkersMax)
+	q.workers = make([]*worker, q.WorkersMax)
 	var i uint32
 	for i = 0; i < q.WorkersMax; i++ {
-		q.w[i] = &worker{
+		q.workers[i] = &worker{
 			status: wstatusIdle,
 			proc:   q.proc,
 		}
-		q.c[i] = make(ctl)
+		q.ctl[i] = make(ctl)
 	}
 	for i = 0; i < q.WorkersMin; i++ {
-		go q.w[i].observe(q.stream, q.c[i])
-		q.c[i] <- signalInit
+		go q.workers[i].observe(q.stream, q.ctl[i])
+		q.ctl[i] <- signalInit
 	}
-	q.wc = q.WorkersMin
+	q.workerUp = q.WorkersMin
+
+	if q.Heartbeat == 0 {
+		q.Heartbeat = defaultHeartbeat
+	}
+	tickerHB := time.NewTicker(q.Heartbeat)
+	go func() {
+		for {
+			select {
+			case <-tickerHB.C:
+				q.rebalance()
+			}
+		}
+	}()
 
 	q.status = qstatusActive
 }
 
 func (q *BalancedQueue) rebalance() {
-	if q.status == qstatusNil {
-		q.once.Do(q.init)
-	}
-
+	q.mux.Lock()
 	rate := q.lcRate()
 	switch {
 	case rate >= q.WakeupFactor:
@@ -87,6 +115,7 @@ func (q *BalancedQueue) rebalance() {
 	default:
 		q.status = qstatusActive
 	}
+	q.mux.Unlock()
 }
 
 func (q *BalancedQueue) lcRate() float32 {
