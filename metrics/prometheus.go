ab1e709f2faf7d1c9cb90a5ba7ee910c43be000d
Konstantin Voykov <koykov0@gmail.com>
2020-12-13 22:21:57 +0200

    Add exact metrics writer for Prometheus.

---
+++ metrics/prometheus.go
@@ -0,0 +1,21 @@
+package metrics
+
+type Prometheus struct {
+	// ...
+}
+
+func (m *Prometheus) WorkerSleep(uint32 int) {
+	//
+}
+
+func (m *Prometheus) WorkerWakeup(uint32 int) {
+	//
+}
+
+func (m *Prometheus) QueuePut() {
+	//
+}
+
+func (m *Prometheus) QueuePull() {
+	//
+}
