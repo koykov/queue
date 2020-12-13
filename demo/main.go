bfa565a3662ce4f302fa3e8d626bf9ac68ac489a
Konstantin Voykov <koykov0@gmail.com>
2020-12-13 22:10:30 +0200

    Demo app issue.

--- demo/main.go
+++ demo/main.go
@@ -1,26 +1,34 @@
 package main
 
 import (
+	"flag"
 	"log"
-	"net/http"
 	"time"
 )
 
 var (
-	workersMin, workersMax    uint32
-	WakeupFactor, SleepFactor float32
-	Heartbeat                 time.Duration
+	fWmin   = flag.Uint("wmin", 0, "Minimal workers count")
+	fWmax   = flag.Uint("wmax", 0, "Maximum workers count")
+	fWakeup = flag.Float64("wakeup", .7, "Wakeup worker factor")
+	fSleep  = flag.Float64("sleep", .5, "Sleep worker factor")
+	fHb     = flag.Int64("hb", 0, "Heartbeat delay")
 
-	qhandler QueueHTTP
+	workersMin, workersMax    uint32
+	wakeupFactor, sleepFactor float32
+	Heartbeat                 time.Duration
 )
 
+func init() {
+	flag.Parse()
+
+	workersMin = uint32(*fWmin)
+	workersMax = uint32(*fWmax)
+	wakeupFactor = float32(*fWakeup)
+	sleepFactor = float32(*fSleep)
+	Heartbeat = time.Duration(*fHb)
+}
+
 func main() {
-	srv := &http.Server{
-		Addr:    ":8080",
-		Handler: &qhandler,
-	}
-	err := srv.ListenAndServe()
-	if err != nil {
-		log.Fatal("err", err)
-	}
+	log.Printf("queue demo\noptions:\n * workers min limit - %d\n * workers max limit - %d\n * wakeup factor - %f\n * sleep factor - %f\n * heartbeat - %d",
+		workersMin, workersMax, wakeupFactor, sleepFactor, Heartbeat)
 }
