0296546b2e2bf3d61aa56da14b4b912d979a423c
Konstantin Voykov <koykov0@gmail.com>
2020-12-14 21:27:31 +0200

    Add new log metrics worker.

---
+++ metrics/log.go
@@ -0,0 +1,21 @@
+package metrics
+
+import "log"
+
+type Log struct{}
+
+func (m *Log) WorkerSleep(idx uint32) {
+	log.Printf("worker %d caught sleep signal\n", idx)
+}
+
+func (m *Log) WorkerWakeup(idx uint32) {
+	log.Printf("worker %d caught sleep signal\n", idx)
+}
+
+func (m *Log) QueuePut() {
+	log.Println("new item come to the queue")
+}
+
+func (m *Log) QueuePull() {
+	log.Println("item leave the queue")
+}
