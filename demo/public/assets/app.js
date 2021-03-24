dd09d447564ba1383e33bd64b6e7eeb5c5921b2e
Konstantin Voykov <koykov0@gmail.com>
2021-03-24 23:12:05 +0200

    Simplify login code.

--- demo/public/assets/app.js
+++ demo/public/assets/app.js
@@ -1,19 +1,8 @@
 class App {
-    #apiUrl = "";
+    apiUrl = "";
     code = 0;
 
     setApiUrl(apiUrl) {
-        this.#apiUrl = apiUrl
-    }
-
-    ping() {
-        let worker = new Worker("assets/worker-ping.js");
-        worker.onmessage = this.pingReceive
-        worker.postMessage(this.#apiUrl + "ping")
-        return this.code;
-    }
-
-    pingReceive(event) {
-        this.code = event.data;
+        this.apiUrl = apiUrl
     }
 }
