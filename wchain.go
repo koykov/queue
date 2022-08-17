e8b941acd2da98330f404dc1f0d8170e3c98949d
Konstantin Voykov <koykov0@gmail.com>
2022-08-18 00:02:08 +0300

    Comment issues: workers chain.

--- wchain.go
+++ wchain.go
@@ -3,11 +3,14 @@ package blqueue
 // WorkersChain describes workers list to process item consecutively.
 type WorkersChain []Worker
 
+// BindWorkers holds workers to one chain.
 func BindWorkers(workers ...Worker) *WorkersChain {
 	wc := WorkersChain(workers)
 	return &wc
 }
 
+// Do process the item.
+// Each worker in chain will be called for processing. Chain will stop processing on first failed worker.
 func (w WorkersChain) Do(x interface{}) (err error) {
 	for i := 0; i < len(w); i++ {
 		if err = w[i].Do(x); err != nil {
