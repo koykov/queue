21b26f4e987e93d952245a932779bd8d81c6c4f1
Konstantin Voykov <koykov0@gmail.com>
2023-05-16 23:23:45 +0300

    QoS: improve weighted priority evaluator; cover with test/bench.

--- priority/weighted.go
+++ priority/weighted.go
@@ -9,52 +9,55 @@ type Weighted struct {
 	Default uint
 	Max     uint
 
-	once     sync.Once
-	max, def uint
+	once sync.Once
+	max  float64
+	def  uint
 }
 
 func (e *Weighted) Eval(weight any) uint {
 	e.once.Do(e.init)
-	var w uint
+	var w float64
 	switch weight.(type) {
 	case int:
-		w = uint(weight.(int))
+		w = float64(weight.(int))
 	case int8:
-		w = uint(weight.(int8))
+		w = float64(weight.(int8))
 	case int16:
-		w = uint(weight.(int16))
+		w = float64(weight.(int16))
 	case int32:
-		w = uint(weight.(int32))
+		w = float64(weight.(int32))
 	case int64:
-		return uint(weight.(int64))
+		w = float64(weight.(int64))
 
 	case uint:
-		w = weight.(uint)
+		w = float64(weight.(uint))
 	case uint8:
-		w = uint(weight.(uint8))
+		w = float64(weight.(uint8))
 	case uint16:
-		w = uint(weight.(uint16))
+		w = float64(weight.(uint16))
 	case uint32:
-		w = uint(weight.(uint32))
+		w = float64(weight.(uint32))
 	case uint64:
-		w = uint(weight.(uint64))
+		w = float64(weight.(uint64))
 
 	case float32:
-		w = uint(weight.(float32))
+		w = float64(weight.(float32))
 	case float64:
-		w = uint(weight.(float64))
+		w = weight.(float64)
 	}
 
 	if w == 0 {
-		w = e.Default
+		return e.def
 	}
 	if w >= e.max {
 		return 100
 	}
-	return w / e.max * 100
+	return uint(w / e.max * 100)
 }
 
 func (e *Weighted) init() {
-	e.max = e.Max
+	if e.max = float64(e.Max); e.max == 0 {
+		e.max = 100
+	}
 	e.def = e.Default
 }
