a19223bdeae5df6186a199e451066a91fa74db1b
Konstantin Voykov <koykov0@gmail.com>
2025-07-10 19:20:40 +0300

    backoff: comments issues

--- backoff/exponential.go
+++ backoff/exponential.go
@@ -7,6 +7,12 @@ import (
 
 // Exponential grows interval by formula `value*2^n`
 // where `n` - number of attempts.
+// Example (for 1 second interval):
+// * 1st step - 1 second
+// * 2nd step - 2 second
+// * 3rd step - 4 second
+// * 4th step - 8 second
+// * ...
 type Exponential struct{}
 
 func (Exponential) Next(interval time.Duration, attempt int) time.Duration {
--- backoff/quadratic.go
+++ backoff/quadratic.go
@@ -7,6 +7,11 @@ import (
 
 // Quadratic grows interval by formula `value*n^2`
 // where `n` - number of attempts.
+// Example (for 1 second interval):
+// * 1st step - 1 second
+// * 2nd step - 4 second
+// * 3rd step - 9 second
+// * ...
 type Quadratic struct{}
 
 func (Quadratic) Next(interval time.Duration, attempt int) time.Duration {
