8b757ac0c95942449897a3d9dee2b51a2a2dbcb8
Konstantin Voykov <koykov0@gmail.com>
2026-07-12 23:34:59 +0300

    pipeline: rework approach

--- pipeline.go
+++ pipeline.go
@@ -1,21 +1,54 @@
 package queue
 
-import "io"
+import (
+	"errors"
+	"io"
+)
 
 type Pipeline interface {
 	io.Closer
 	Enqueuer
 	AddQueue(q *Queue) Pipeline
 	AddAsyncQueue(q *Queue) Pipeline
-	AddPipeline(p Pipeline) Pipeline
-	AddAsyncPipeline(p Pipeline) Pipeline
 }
 
 type pipeline struct {
-	// ...
+	buf []pipeq
+}
+
+type pipeq struct {
+	q     *Queue
+	async bool
+}
+
+func NewPipeline() Pipeline {
+	return &pipeline{}
 }
 
 func (p *pipeline) Enqueue(x any) error {
 	// todo implement me
+	return nil
+}
+
+func (p *pipeline) AddQueue(q *Queue) Pipeline {
+	p.buf = append(p.buf, pipeq{q: q})
+	return p
+}
+
+func (p *pipeline) AddAsyncQueue(q *Queue) Pipeline {
+	p.buf = append(p.buf, pipeq{q: q, async: true})
+	return p
+}
+
+func (p *pipeline) Close() error {
+	var errs []error
+	for i := 0; i < len(p.buf); i++ {
+		if err := p.buf[i].q.Close(); err != nil {
+			errs = append(errs, err)
+		}
+	}
+	if len(errs) > 0 {
+		return errors.Join(errs...)
+	}
 	return nil
 }
